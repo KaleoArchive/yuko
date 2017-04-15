423810ebf96f16f1ad325022131fc138de1719b4
Kaleo Cheng <kaleocheng@gmail.com>
2017-04-16 00:02:12 +0800

    Initial Commit

---
+++ yuko/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  yuko
+//
+//  Created by Kaleo Cheng on 2017/4/16.
+//  Copyright © 2017年 Kaleo Cheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ yuko/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  yuko
+//
+//  Created by Kaleo Cheng on 2017/4/16.
+//  Copyright © 2017年 Kaleo Cheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
